d65e1e346d9815613b7aa439a53df3a369cbdf04
OguzGeylaniYilmaz <49364606+OguzGeylaniYilmaz@users.noreply.github.com>
2022-01-09 18:27:49 +0300

    second commit

---
+++ Metotlat_Overloading/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Metotlar_Overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //out parametreler
+            string sayi = "999";
+
+            bool sonuc = int.TryParse(sayi,out int outSayi);
+            if(sonuc)
+            {
+                Console.WriteLine("Başarılı!");
+                Console.WriteLine(outSayi);
+            }
+            else
+            {
+                Console.WriteLine("Başarısız");
+            }
+
+            Metotlar metotlar = new Metotlar();
+            metotlar.Topla(4,5,out int toplamSonucu);
+            Console.WriteLine(toplamSonucu);
+
+            //Metot Aşırı Yükleme - Overloading
+
+            int ifade = 999;
+            metotlar.EkranaYazdir(Convert.ToInt32(ifade));
+            metotlar.EkranaYazdir(ifade);
+
+        }
+    }
+
+    class Metotlar
+    {
+        public void Topla(int a,int b,out int toplam)
+        {
+            toplam = a + b;
+        }
+
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+         public void EkranaYazdir(int veri)
+        {
+            Console.WriteLine(veri);
+        }
+         public void EkranaYazdir(string veri,string veri2)
+        {
+            Console.WriteLine(veri + veri2);
+        }
+    }
+}
