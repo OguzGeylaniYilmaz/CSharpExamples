fef14a55a9e7ed3b0c6bfc4a0311d8a4d2bdd429
OguzGeylaniYilmaz <49364606+OguzGeylaniYilmaz@users.noreply.github.com>
2022-01-09 21:22:42 +0300

    soru1

---
+++ Odev_1/Soru1/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+
+namespace Soru_1
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Lütfen pozitif bir sayı giriniz = ");
+            int n = int.Parse(Console.ReadLine());
+
+            if (n>0)
+            {
+                Console.WriteLine("Lütfen {0} adet sayı giriniz",n);
+                for (int i = 1; i < n; i++)
+                {
+                    Console.WriteLine("{0}. sayı = " , i);
+                    int sayi = int.Parse(Console.ReadLine());
+                    if(sayi %2 == 0)
+                        Console.WriteLine("Sayı çiftdir = " ,sayi);
+                    else
+                        Console.WriteLine("Sayı tektir = ",sayi);
+                }
+
+            }
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
+  
